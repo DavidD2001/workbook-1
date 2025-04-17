98710244ed12a489b0c2e23f1fe403272ca785d9
David D. <daviddorsey679@yahoo.com>
2025-04-16 20:06:25 -0700

    Added PayrollCalculator Exercise optional

--- PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -4,19 +4,36 @@ public class PayrollCalculator {
     public static void main(String [] args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
+        String name = getEmployeeName(scanner);
+        float hours = getHoursWorked(scanner);
+        float rate = getPayRate(scanner);
+        float grossPay = calculateGrossPay(hours, rate);
 
-        System.out.println("Enter your hours worked: ");
-        float workHours = scanner.nextFloat();
-
-        System.out.println("Enter your pay rate: ");
-        float payRate = scanner.nextFloat();
-
-        float grossPay = workHours * payRate;
-
-        System.out.println("Employee name: " + name);
-        System.out.println("Your Gross pay rate: " + grossPay);
-
+        displayPayStub(name, grossPay);
     }
+public static String getEmployeeName(Scanner scanner){
+        System.out.print("Enter employee name: ");
+        return scanner.nextLine();
 }
+
+public static float getHoursWorked(Scanner scanner){
+        System.out.print("Enter hours worked: ");
+        return scanner.nextFloat();
+}
+
+public static float getPayRate(Scanner scanner){
+    System.out.print("Enter pay rate: ");
+    return scanner.nextFloat();
+    }
+public static float calculateGrossPay(float hours, float rate) {
+       return hours * rate;
+}
+public static void displayPayStub(String name, float grossPay) {
+    System.out.println("\nEmployee name: " + name);
+    System.out.println("Your gross pay is: $" + grossPay);
+}
+public static float calculateTax(float grossPay) {
+        return grossPay * 0.2f; //example: 20% tax
+}
+}
+
