63dbb6b974c0ca48ef0fa988540152c37583d07e
David D. <daviddorsey679@yahoo.com>
2025-06-11 13:09:29 -0700

    completed Shopping List

---
+++ ShoppingList/src/ShoppingList.java
@@ -0,0 +1,15 @@
+public class ShoppingList {
+    public static void main(String[]args) {
+        System.out.println("Shopping List: ");
+        System.out.println("\n1. Eggs");
+        System.out.println("2. Milk");
+        System.out.println("3. Butter");
+        System.out.println("4. Apples");
+        System.out.println("5. Peaches");
+        System.out.println("6. Oranges");
+        System.out.println("7. Bread");
+        System.out.println("8. Water");
+        System.out.println("9. Ground Beef");
+        System.out.println("10. Chicken");
+    }
+}
